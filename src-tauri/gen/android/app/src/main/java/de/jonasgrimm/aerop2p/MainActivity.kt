62268855412b05a419fd5ea148005315394fba11
jonasgrimmde <turtelmaster11@gmail.com>
2026-07-06 13:09:45 +0200

    a

---
+++ src-tauri/gen/android/app/src/main/java/de/jonasgrimm/aerop2p/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.jonasgrimm.aerop2p
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
