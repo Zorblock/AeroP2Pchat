e92ab44e6696dce06274cbb69957fc29be149496
Zorblock <turtelmaster11@gmail.com>
2026-07-08 22:32:25 +0200

    a

--- .pages/vite.config.ts
+++ .pages/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/AeroP2Pchat/',
+  base: process.env.CF_PAGES ? '/' : '/AeroP2Pchat/',
 })
